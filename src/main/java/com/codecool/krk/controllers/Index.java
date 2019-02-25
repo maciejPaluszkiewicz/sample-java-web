3d7a825ffa0764e431d38243191c8dd591e06ddf
maciejPaluszkiewicz <maciej.j.paluszkiewicz@gmail.com>
2019-02-25 22:03:55 +0100

    add index cookie deletion

--- src/main/java/com/codecool/krk/controllers/Index.java
+++ src/main/java/com/codecool/krk/controllers/Index.java
@@ -1,11 +1,20 @@
 package com.codecool.krk.controllers;
 
+import com.codecool.krk.helpers.CookieHelper;
+import com.sun.net.httpserver.HttpExchange;
 import org.jtwig.JtwigModel;
+import java.io.IOException;
 
 public class Index extends TemplateHandler {
 
     public Index() {
         super("templates/index.twig");
+    }
+
+    @Override
+    public void handle(HttpExchange httpExchange) throws IOException {
+        CookieHelper.deleteCookie(httpExchange, CookieHelper.COOKIE_USERNAME);
+        super.handle(httpExchange);
     }
 
     @Override
